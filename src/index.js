de57bd6f26351ffd9245f4c227a377697e67583a
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-06-14 02:05:31 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for (let count = 1; count <= 10; count += 1) {
+  console.log(count);
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let count = 10;
+while (count >= 0) {
+  console.log(count);
+  count -= 1;
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+for (let count = 1; count <= 100; count += 2) {
+  console.log(count);
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+for (let count = 0; count <= 100; count += 2) {
+  console.log(count);
+}
